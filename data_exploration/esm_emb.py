017f83186bb70e55ceb5caf226bdefb683e8c0ad
Queimo <queimo@gmx.net>
2024-12-09 16:34:42 +0100

    esm-emb

---
+++ data_exploration/esm_emb.py
@@ -0,0 +1,14 @@
+from transformers import AutoTokenizer, EsmModel
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t6_8M_UR50D")
+model = EsmModel.from_pretrained("facebook/esm2_t6_8M_UR50D")
+
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+outputs = model(**inputs)
+
+last_hidden_states = outputs.last_hidden_state
+
+print(outputs)
+print(last_hidden_states)
+print(last_hidden_states.shape)
