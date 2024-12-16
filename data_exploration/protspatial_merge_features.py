0d6e83b388be66d5b6a067c2bfac46452ac2f429
Karim Ben Hicham <karim.ben.hicham@rwth-aachen.de>
2024-12-16 15:18:31 +0100

    path

--- data_exploration/protspatial_merge_features.py
+++ data_exploration/protspatial_merge_features.py
@@ -5,11 +5,9 @@ model_name = "esm2_t6_8M_UR50D"
 model_path = "facebook/" + model_name
 
 # 2. Load and preprocess the dataset
-# df = pd.read_csv("./data_exploration/data/ProtSpatial/protein_data raw.csv", index_col=0)
-df = pd.read_csv("./data/ProtSpatial/protein_data raw.csv", index_col=0)
+df = pd.read_csv("./data_exploration/data/ProtSpatial/protein_data raw.csv", index_col=0)
 
-# csv_path = f"./data_exploration/data/ProtSpatial/{model_name}_embeddings.csv"
-csv_path = f"./data/ProtSpatial/{model_name}_embeddings.csv"
+csv_path = f"./data_exploration/data/ProtSpatial/{model_name}_embeddings.csv"
 # merge data
 df_emb = pd.read_csv(csv_path)
 df.columns
@@ -20,4 +18,4 @@ df_emb.columns
 #merge on variant
 df_final = df.merge(df_emb, on='variant')
 df_final.nunique()
-df_final.to_csv(f"./data/ProtSpatial/protein_data{model_name}_final.csv")
+df_final.to_csv(f"./data_exploration/data/ProtSpatial/protein_data{model_name}_final.csv")
